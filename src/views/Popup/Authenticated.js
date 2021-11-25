6908eaa82b21a9e21debfc28b5d9abae17e6d1a3
valdis aglonietis <valdisaglonietis@gmail.com>
2021-11-26 00:36:53 +0200

    Removed options from old code that was used as base of the codebase

--- src/views/Popup/Authenticated.js
+++ src/views/Popup/Authenticated.js
@@ -2,49 +2,13 @@ import React, {Component} from 'react';
 import {Header, Icon, Label, Placeholder} from 'semantic-ui-react';
 
 export default class Authenticated extends Component {
-    onSettings = (e) => {
-        e.preventDefault();
-        chrome.runtime.openOptionsPage && chrome.runtime.openOptionsPage();
-    }
-
-    onLogout = (e) => {
-        e.preventDefault();
-        const {accountLogout} = this.props;
-        accountLogout();
-    }
-
-    onCheck = (e, {checked}) => {
-        e.preventDefault();
-        const {setEnabled, setStats} = this.props;
-        setEnabled(checked);
-        !checked && setStats(false);
-    }
-
     render() {
-        const {name, keywords, stats,firstName, lastName} = this.props;
+        const {firstName, lastName} = this.props;
         return (
             <div>
-                {!name && !keywords &&
-                <Placeholder fluid>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                    <Placeholder.Line/>
-                </Placeholder>
-                }
-
                 <Header as='h4'>
                     <Icon name='user'/>{firstName} {lastName}
                 </Header>
-
-                {keywords && keywords.map((v, i) =>
-                    <Label color='red' tag>
-                        {v}
-                        {stats &&
-                        <Label.Detail>{stats[i]}</Label.Detail>
-                        }
-                    </Label>
-                )}
             </div>
         );
     }
--- src/views/Popup/Unauthenticated.js
+++ src/views/Popup/Unauthenticated.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
 
 export default class Unauthenticated extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
-        const {apiUrl} = this.props;
-
         return (
             <div>
-                <label>{apiUrl} Your configuration has incomplete! Please visit options and finish configuration!</label>
+                <label>Your configuration has incomplete! Please visit options and finish configuration!</label>
             </div>
         );
     }
